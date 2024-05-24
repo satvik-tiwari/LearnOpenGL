33af140f22c3f7d28887ad4d562cb6af7389ee23
Kivtas <satviktiwarikivtas7@gmail.com>
2024-05-24 14:21:52 +0100

    made a function const

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -16,7 +16,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, int> m_UniformLocationCache;
+	mutable std::unordered_map<std::string, int> m_UniformLocationCache;
 	//caching for uniforms
 
 public:
@@ -33,7 +33,7 @@ public:
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
-    int GetUniformLoacation(const std::string& name);
+    int GetUniformLoacation(const std::string& name) const;
 
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
