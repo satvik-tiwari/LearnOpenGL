d37f877fe1055b0394688e1fc788568bc1bc727e
Kivtas <satviktiwarikivtas7@gmail.com>
2024-06-01 14:40:37 +0100

    changed window name

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -39,7 +39,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     //window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
